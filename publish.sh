17fe983a9b6de9b4c2a1ac2cd2d47a677fcaa744
zooter <zzc1368129224@qq.com>
2022-03-16 20:00:19 -0700

    Update pubish.sh. Add iptables related content.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,13 @@
 #/bin/bash
 
-rm -rf ./docs
+if [ -d "public" ];
+then
+	echo 'Delete public folder.'
+	rm -rf public
+fi
+if [ -d "docs" ];
+then
+	echo 'Delete docs folder.'
+	rm -rf docs
+fi
 hugo -d docs
