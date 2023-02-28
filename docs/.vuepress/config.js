278e9648f6b37589f4a14e5573b33fed43b6a63e
Zhichao Zeng <zhichaox.zeng@intel.com>
2023-02-28 15:26:15 +0800

    change to vuepress-hope-theme

--- docs/.vuepress/config.js
+++
@@ -1,9 +0,0 @@
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  lang: 'zh-CN',
-  title: 'zooter',
-  description: '这是我的第一个 VuePress 站点',
-  theme: hopeTheme({
-  }),
-})
